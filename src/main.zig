b04c922270f648d2496c3c0e7fc8d5c41cd60d31
Tau <tauoverpi@yandex.com>
2020-10-26 22:13:04 +0100

    Restructure timer interface with get/set

--- src/main.zig
+++ src/main.zig
@@ -10,33 +10,31 @@ pub fn main() noreturn {
     const led = pine.GpioPin.p0_17;
     const timer = pine.Timer.timer0;
 
+    led.config(.{
+        .direction = .output,
+        .input = .disconnect,
+        .pull = .disabled,
+        .drive = .s0s1,
+        .sense = .disabled,
+    });
+
     while (true) {
-        led.config(.{
-            .direction = .output,
-            .input = .disconnect,
-            .pull = .disabled,
-            .drive = .s0s1,
-            .sense = .disabled,
-        });
-
-        timer.config(.{ .mode = 0 });
-        timer.config(.{ .bitmode = 3 });
-        timer.config(.{ .prescaler = 0 });
-        timer.config(.{ .cc_0 = 0 });
-        timer.config(.{ .compare_0 = 0 });
-        timer.config(.clear);
-        timer.config(.start);
-        while (@intToPtr(*volatile u32, @enumToInt(timer) + @enumToInt(pine.Timer.TimerConfig.compare_0)).* == 0) {}
-
+        timer.setMode(.timer);
+        timer.setBitMode(.b32);
+        timer.setPrescaler(0);
+        timer.setCaptureCompare(.cc_0, 500);
+        timer.clearEvent(.event_0);
+        timer.clear();
+        timer.start();
         pine.Gpio.set(.{ .p0_17 = true });
 
-        timer.config(.{ .mode = 0 });
-        timer.config(.{ .bitmode = 3 });
-        timer.config(.{ .prescaler = 0 });
-        timer.config(.{ .cc_0 = 0 });
-        timer.config(.{ .compare_0 = 0 });
-        timer.config(.clear);
-        timer.config(.start);
-        while (@intToPtr(*volatile u32, @enumToInt(timer) + @enumToInt(pine.Timer.TimerConfig.compare_0)).* == 0) {}
+        timer.setMode(.timer);
+        timer.setBitMode(.b32);
+        timer.setPrescaler(0);
+        timer.setCaptureCompare(.cc_0, 500);
+        timer.clearEvent(.event_0);
+        timer.clear();
+        timer.start();
+        pine.Gpio.set(.{ .p0_17 = false });
     }
 }
--- src/timer.zig
+++ src/timer.zig
@@ -5,109 +5,159 @@ pub const Timer = enum(u32) {
     timer3 = 0x4001a000,
     timer4 = 0x4001b000,
 
-    pub const TimerConfig = union(enum(u24)) {
-        start = 0x000,
-        stop = 0x004,
-        increment = 0x008,
-        clear = 0x00c,
-
-        capture_0 = 0x010,
-        capture_1 = 0x040,
-        capture_2 = 0x044,
-        capture_3 = 0x04c,
-        capture_4 = 0x050,
-        capture_5 = 0x054,
-
-        compare_0: u32 = 0x140,
-        compare_1: u32 = 0x144,
-        compare_2: u32 = 0x148,
-        compare_3: u32 = 0x14c,
-        compare_4: u32 = 0x150,
-        compare_5: u32 = 0x154,
-
-        shortcut: Shortcut = 0x200,
-        interrupt_enable: Interrupt = 0x304,
-        interrupt_disable: Interrupt = 0x308,
-        mode: u32 = 0x504,
-        bitmode: u32 = 0x508,
-        prescaler: u32 = 0x510,
-
-        cc_0: u32 = 0x540,
-        cc_1: u32 = 0x544,
-        cc_2: u32 = 0x548,
-        cc_3: u32 = 0x54c,
-        cc_4: u32 = 0x550,
-        cc_5: u32 = 0x554,
-
-        pub const Shortcut = packed struct {
-            compare_clear_0: bool = false,
-            compare_clear_1: bool = false,
-            compare_clear_2: bool = false,
-            compare_clear_3: bool = false,
-            compare_clear_4: bool = false,
-            compare_clear_5: bool = false,
-
-            _unused1: u2 = 0,
-
-            compare_stop_0: bool = false,
-            compare_stop_1: bool = false,
-            compare_stop_2: bool = false,
-            compare_stop_3: bool = false,
-            compare_stop_4: bool = false,
-            compare_stop_5: bool = false,
-            _unused2: u2 = 0,
-            _unused21: u16 = 0,
-        };
-
-        pub const Interrupt = packed struct {
-            _unused1: u16 = 0,
-            compare_0: bool = false,
-            compare_1: bool = false,
-            compare_2: bool = false,
-            compare_3: bool = false,
-            compare_4: bool = false,
-            compare_5: bool = false,
-            _unused2: u10 = 0,
-        };
+    pub const Interrupt = packed struct {
+        _unused1: u16 = 0,
+        compare_0: bool = false,
+        compare_1: bool = false,
+        compare_2: bool = false,
+        compare_3: bool = false,
+        compare_4: bool = false,
+        compare_5: bool = false,
+        _unused2: u10 = 0,
     };
 
-    pub fn config(timer: Timer, cfg: TimerConfig) void {
-        const address = @intToPtr(*volatile u32, @enumToInt(timer) + @enumToInt(cfg));
-        switch (cfg) {
-            .start,
-            .stop,
-            .increment,
-            .clear,
-            .capture_0,
-            .capture_1,
-            .capture_2,
-            .capture_3,
-            .capture_4,
-            .capture_5,
-            => address.* = 1,
-
-            .shortcut => |settings| address.* = @bitCast(u32, settings),
-
-            .interrupt_enable,
-            .interrupt_disable,
-            => |settings| address.* = @bitCast(u32, settings),
-
-            .mode,
-            .bitmode,
-            .prescaler,
-            .cc_0,
-            .cc_1,
-            .cc_2,
-            .cc_3,
-            .cc_4,
-            .cc_5,
-            .compare_0,
-            .compare_1,
-            .compare_2,
-            .compare_3,
-            .compare_4,
-            .compare_5,
-            => |settings| address.* = settings,
-        }
+    pub fn setInterrupt(timer: Timer, cfg: Interrupt) void {
+        const offset = 0x304;
+        const address = @intToPtr(*volatile u32, @enumToInt(timer) + offset);
+        address.* = @bitCast(u32, cfg);
     }
+
+    pub fn clearInterrupt(timer: Timer, cfg: Interrupt) void {
+        const offset = 0x308;
+        const address = @intToPtr(*volatile u32, @enumToInt(timer) + offset);
+        address.* = @bitCast(u32, cfg);
+    }
+
+    pub fn start(timer: Timer) void {
+        const address = @intToPtr(*volatile u32, @enumToInt(timer));
+        address.* = 1;
+    }
+
+    pub fn stop(timer: Timer) void {
+        const offset = 0x004;
+        const address = @intToPtr(*volatile u32, @enumToInt(timer));
+        address.* = 1;
+    }
+
+    pub fn increment(timer: Timer) void {
+        const offset = 0x008;
+        const address = @intToPtr(*volatile u32, @enumToInt(timer));
+        address.* = 1;
+    }
+
+    pub fn clear(timer: Timer) void {
+        const offset = 0x00c;
+        const address = @intToPtr(*volatile u32, @enumToInt(timer));
+        address.* = 1;
+    }
+
+    pub const TimerCapture = enum(u32) {
+        cc_0 = 0x010,
+        cc_1 = 0x040,
+        cc_2 = 0x044,
+        cc_3 = 0x04c,
+        cc_4 = 0x050,
+        cc_5 = 0x054,
+    };
+
+    pub fn setCapture(timer: Timer, cap: TimerCapture, value: u32) void {
+        const offset = @enumToInt(cap);
+        const address = @intToPtr(*volatile u32, @enumToInt(timer) + offset);
+        address.* = value;
+    }
+
+    pub const TimerEvent = enum(u32) {
+        event_0 = 0x140,
+        event_1 = 0x144,
+        event_2 = 0x148,
+        event_3 = 0x14c,
+        event_4 = 0x150,
+        event_5 = 0x154,
+    };
+
+    pub fn clearEvent(timer: Timer, event: TimerEvent) void {
+        const offset = @enumToInt(event);
+        const address = @intToPtr(*volatile u32, @enumToInt(timer) + offset);
+        address.* = 0;
+    }
+
+    pub const Shortcut = packed struct {
+        clear_0: bool = false,
+        clear_1: bool = false,
+        clear_2: bool = false,
+        clear_3: bool = false,
+        clear_4: bool = false,
+        clear_5: bool = false,
+
+        _unused1: u2 = 0,
+
+        stop_0: bool = false,
+        stop_1: bool = false,
+        stop_2: bool = false,
+        stop_3: bool = false,
+        stop_4: bool = false,
+        stop_5: bool = false,
+        _unused2: u2 = 0,
+        _unused21: u16 = 0,
+    };
+
+    pub fn set(timer: Timer, short: Shortcut) void {
+        const offset = 0x200;
+        const address = @intToPtr(*volatile u32, @enumToInt(timer) + offset);
+        address.* = @bitCast(u32, short);
+    }
+
+    pub const TimerMode = enum(u32) {
+        timer = 0,
+        counter = 1,
+        low_power_counter = 2,
+    };
+
+    pub fn setMode(timer: Timer, mode: TimerMode) void {
+        const offset = 0x504;
+        const address = @intToPtr(*volatile u32, @enumToInt(timer) + offset);
+        address.* = @enumToInt(mode);
+    }
+
+    pub const TimerBitMode = enum(u32) {
+        b16 = 0,
+        b8 = 1,
+        b24 = 2,
+        b32 = 3,
+    };
+
+    pub fn setBitMode(timer: Timer, bit: TimerBitMode) void {
+        const offset = 0x508;
+        const address = @intToPtr(*volatile u32, @enumToInt(timer) + offset);
+        address.* = @enumToInt(bit);
+    }
+
+    pub fn setPrescaler(timer: Timer, scale: u32) void {
+        const offset = 0x510;
+        const address = @intToPtr(*volatile u32, @enumToInt(timer) + offset);
+        address.* = scale;
+    }
+
+    pub const TimerCaptureCompare = enum(u32) {
+        cc_0 = 0x540,
+        cc_1 = 0x544,
+        cc_2 = 0x548,
+        cc_3 = 0x54c,
+        cc_4 = 0x550,
+        cc_5 = 0x554,
+    };
+
+    pub fn setCaptureCompare(timer: Timer, cc: TimerCaptureCompare, value: u32) void {
+        const offset = @enumToInt(cc);
+        const address = @intToPtr(*volatile u32, @enumToInt(timer) + offset);
+        address.* = value;
+    }
+
+    // TODO
+    pub const TimerConfig = struct {
+        set_interrupt: ?Interrupt = null,
+    };
+
+    // TODO
+    pub fn configure() void {}
 };
